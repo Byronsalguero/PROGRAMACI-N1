7886b2a617be50af04c32f8a662d273a73e7d72e
Byronsalguero <79301316+Byronsalguero@users.noreply.github.com>
2021-02-19 19:31:36 -0600

    Add files via upload

---
+++ mayuscula/mayuscula/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace mayuscula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("PRESIONE UN CARACTER: ");
+            char c = (char)Console.Read();
+            if (Char.IsLetter(c))
+            {
+                if (Char.IsLower(c))
+                {
+                    Console.WriteLine("EL CARACTER ESTA EN MINUSCULA.");
+                }
+                else
+                {
+                    Console.WriteLine("EL CARACTER ESTA EN MAYUSCULA.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("EL CARACTER NO ES UN CARACTER ALFABÉTICO.");
+            }
+        }
+    }
+}

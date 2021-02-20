0a704a952bfd49f89403935af0e10586cba43935
Byronsalguero <79301316+Byronsalguero@users.noreply.github.com>
2021-02-19 19:23:50 -0600

    Add files via upload

---
+++ descuento/descuento/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace descuento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int total, pago;
+            Console.WriteLine("ingrese una cantidad de dinero:");
+
+            int compra = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("ingrese el porcentaje de descuento");
+            int descuento = Convert.ToInt32(Console.ReadLine());
+
+            total = compra * descuento / 100;
+            pago = compra - descuento;
+            Console.WriteLine("total a pagar:" + pago);
+
+            if (pago >= 300)
+                Console.WriteLine(pago = compra - descuento);
+
+            else if (pago < 300)
+                Console.WriteLine("NO HAY DESCUENTO");
+      
+            }
+        }
+    }
+
+

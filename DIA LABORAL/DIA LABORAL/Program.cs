32fd7e0fbe1361ce3c7742b240c8af6e0058ea3d
Byronsalguero <79301316+Byronsalguero@users.noreply.github.com>
2021-02-19 19:27:25 -0600

    Add files via upload

---
+++ DIA LABORAL/DIA LABORAL/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace DIA_LABORAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dia;
+            Console.WriteLine("introduce un DIA de la Semana");
+            dia = Console.ReadLine();
+            switch(dia)
+            {
+
+                case "LUNES":
+                Console.WriteLine("DIA LABORAL");
+                    break;
+
+                case "MARTES":
+                    Console.WriteLine("NO ES DIA LABORAL");
+                    break;
+
+                case "MIERCOLES":
+                    Console.WriteLine("DIA LABORAL");
+                    break;
+
+
+                case "JUEVES":
+                    Console.WriteLine("NO ES DIA LABORAL");
+                    break;
+
+                case "VIERNES":
+                    Console.WriteLine("DIA LABORAL");
+                    break;
+
+                case "SABADO":
+                    Console.WriteLine("NO ES DIA LABORAL");
+                    break;
+
+                case "DOMINGO":
+                    Console.WriteLine("NO ES DIA LABORAL");
+                    break;
+
+                default:
+                    Console.WriteLine("INTRODUZCA UN DIA PORFAVOR");
+                    break;
+
+
+
+
+
+            }
+        }
+    }
+}

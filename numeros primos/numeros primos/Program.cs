eee8cc6981d88ffcd8b7ec77deaf2e775f29d18a
Byronsalguero <79301316+Byronsalguero@users.noreply.github.com>
2021-02-26 19:22:28 -0600

    Add files via upload

---
+++ numeros primos/numeros primos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace numeros_primos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, cont = 1;
+            Console.WriteLine("Introduce la cantidad de numeros primos que deseas:");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; cont <= num; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(cont + "ESTE ES UN NUMERO PRIMO:" + i);
+                    cont++;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
---
+++ potencia/potencia/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace potencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 2, potencia = 4;
+            double elevado = Math.Pow(numero, potencia); 
+            Console.WriteLine(string.Format("{0} elevado a la potencia {1} es {2}", numero, potencia, elevado));
+
+        }
+    }
+}
